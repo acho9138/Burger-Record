717c5af9b918fccdbf76a8976b3dae7d356dcb24
Anna C <anna.s.chong@gmail.com>
2020-08-15 22:28:39 +1000

    ADD api route to POST submitted burger to db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -9,10 +9,16 @@ router.get("/", (req, res) => {
   });
 });
 
+router.post("/api/burgers", (req, res) => {
+  burger.insertOne(req.body.burger_name, (data) => {
+    res.json({ id: data.insertId });
+  });
+});
+
 router.put("/api/burgers/:id", (req, res) => {
   burger.updateOne(req.params.id, (result) => {
+    // If no rows were changed, then the ID must not exist, so 404
     if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
       return res.status(404).end();
     } else {
       res.status(200).end();
