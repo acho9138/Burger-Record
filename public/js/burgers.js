b459b7e19dc90c73bc772c73a003a17738f4581f
Anna C <anna.s.chong@gmail.com>
2020-08-15 22:27:50 +1000

    ADD api call to POST new burger to db

--- public/js/burgers.js
+++ public/js/burgers.js
@@ -1,9 +1,26 @@
 $(() => {
+  // API call to update devoured column to true when button is clicked
   $(".devour_burger").on("click", function (event) {
     const id = $(this).data("id");
 
     $.ajax("/api/burgers/" + id, {
       type: "PUT",
+    }).then(() => {
+      location.reload();
+    }
+    );
+  });
+
+  // API call to post new burger into database
+  $("#addBurger").on("submit", (event) => {
+    event.preventDefault();
+
+    const newBurger = {
+      burger_name: $("#burger_name").val().trim()
+    };
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger
     }).then(() => {
       location.reload();
     }
