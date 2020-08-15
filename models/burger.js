359ccb1b9d64e9168163b00f576a1532b64497e3
Anna C <anna.s.chong@gmail.com>
2020-08-15 15:36:52 +1000

    ADD burger orm model

---
+++ models/burger.js
@@ -0,0 +1,21 @@
+const orm = require("../config/orm.js");
+
+const burger = {
+  selectAll: (callback) => {
+    orm.selectAll("burgers", (res) => {
+      callback(res);
+    })
+  },
+  insertOne: (burgerName, callback) => {
+    orm.insertOne("burgers", "burger_name", "devoured", burgerName, false, (res) => {
+      callback(res);
+    })
+  },
+  updateOne: (selectedId, callback) => {
+    orm.updateOne("burgers", "devoured", 1, "id", selectedId, (res) => {
+      callback(res);
+    })
+  }
+};
+
+module.exports = burger;
